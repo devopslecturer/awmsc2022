f36b2ad50b6a24e82059f0c718cc344772511fc1
MShafeeq <L00163279@student.lyit.ie>
2022-02-08 18:33:30 +0000

    flask initial setup

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app = Flask(__name__)
 
 
+# app.register_blueprint(user_bp, url_prefix='/users')
+# app.register_blueprint(signup_bp, url_prefix='/signup')
+# app.register_blueprint(login_bp, url_prefix='/login')
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
