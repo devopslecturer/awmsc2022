0e9bb5f0aade922a84221a8e0065d5985a26bf3c
Anup Jacob <L00163455@student.lyit.ie>
2022-03-15 15:05:25 +0000

    Restructuring files

--- app.py
+++ app.py
@@ -1,52 +1,43 @@
 from flask import Flask, render_template, request
-# from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 
+
 app = Flask(__name__)
-mysql = MySQL(app)
+
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'wedwckd'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flask'
 
-# app.register_blueprint(user_bp, url_prefix='/users')
-# app.register_blueprint(signup_bp, url_prefix='/signup')
-# app.register_blueprint(login_bp, url_prefix='/login')
-
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return render_template('html/Home.html')
+    return render_template('/Wed_wicked_adv_code/template/Home.html')
 
-@app.route('/login_signup')
-def load_login_page():
-    return render_template('html/login_signup.html')
 
-@app.route('/login_signup_check', methods = ['GET','POST'])
-def login_signup():
-    if request.method == 'GET':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        cursor = mysql.connection.cursor()
-        result = cursor.execute('''select * from cust_details where email=%s and Password=%s''',(email, password))
-        # cursor.execute(''' INSERT INTO cust_details VALUES(%s,%s,%s)''',(FullName,Email,Password))
-        mysql.connection.commit()
-        cursor.close()
-        print(email)
-        print(password)
-        return '</h1>logged in</h2>'
+@app.route('/Wed_wicked_adv_code/template/login-signup.html', methods=['GET'])
+def login():
 
+    return render_template('/Wed_wicked_adv_code/template/login-signup.html')
+
+
+@app.route('/Wed_wicked_adv_code/template/login-signup.html', methods=['POST'])
+def sign_up():
     if request.method == 'POST':
-        firstname = request.form.get('su-name')
+        FullName = request.form.get('su-name')
+        print(FullName)
         Email = request.form.get('su-mail')
         Password = request.form.get('su-pass')
-        lastname=''
         cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO cust_details VALUES(%s,%s,%s,%s)''',(firstname,lastname,Email,Password))
+        cursor.execute(
+            ''' INSERT INTO cust_details VALUES(%s,%s,%s)''', (FullName, Email, Password))
         mysql.connection.commit()
         cursor.close()
-        return '</h1>signed up</h2>'    
-        
+        return render_template('/Wed_wicked_adv_code/template/login-signup.html')
+
+    return render_template('/Wed_wicked_adv_code/template/login-signup.html')
+
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(host='localhost', port=5501)
