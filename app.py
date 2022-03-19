5e7ca28f412306201911d03d0661b1ceb566ff01
VRL00163087 <l00163087@student.lyit.ie>
2022-03-19 04:19:40 -0700

    login functionaltty, structure cleanup, update links

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 # from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 mysql = MySQL(app)
@@ -9,40 +10,44 @@ app.config['MYSQL_USER'] = 'admin'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'wedwckd'
 
-# app.register_blueprint(user_bp, url_prefix='/users')
-# app.register_blueprint(signup_bp, url_prefix='/signup')
-# app.register_blueprint(login_bp, url_prefix='/login')
-
-
 @app.route('/')
 def index():
-    return render_template('html/Home.html')
+    return render_template('home.html')
 
 @app.route('/login_signup')
 def load_login_page():
-    return render_template('html/login_signup.html')
+    return render_template('login_signup.html')
 
 @app.route('/login_signup_check', methods = ['GET','POST'])
 def login_signup():
     if request.method == 'GET':
-        email = request.form.get('email')
-        password = request.form.get('password')
+        # print(request.args)
+        email = request.args.get('email')
+        password = request.args.get('password')
         cursor = mysql.connection.cursor()
-        result = cursor.execute('''select * from cust_details where email=%s and Password=%s''',(email, password))
-        # cursor.execute(''' INSERT INTO cust_details VALUES(%s,%s,%s)''',(FullName,Email,Password))
-        mysql.connection.commit()
-        cursor.close()
-        print(email)
-        print(password)
-        return '</h1>logged in</h2>'
+        print('email',email, 'password', password)
+        cursor.execute('''select * from cust_details where email=%s and Password=%s''',(email,password))
+        result = cursor.fetchone()
+        # print('result',result)
+        if None != result and email == result[2] and password == result[3]:
+            mysql.connection.commit()
+            cursor.close()
+            return render_template('profile.html')
+        else: 
+            mysql.connection.commit()
+            cursor.close()
+            return '<h1>not matched</h1>'
 
     if request.method == 'POST':
-        firstname = request.form.get('su-name')
-        Email = request.form.get('su-mail')
-        Password = request.form.get('su-pass')
-        lastname=''
+        firstname = request.args.get('su-name')
+        lastname=request.args.get('su-lastname')
+        Email = request.args.get('su-mail')
+        Password = request.args.get('su-pass')  
+        # print(Email)
+        # print(Password)
+        # print(lastname)
         cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO cust_details VALUES(%s,%s,%s,%s)''',(firstname,lastname,Email,Password))
+        cursor.execute('''INSERT INTO cust_details VALUES(%s,%s,%s,%s)''',(firstname,lastname,Email,Password))
         mysql.connection.commit()
         cursor.close()
         return '</h1>signed up</h2>'    
