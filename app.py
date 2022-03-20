c160135fa84e15c44638dfea2d5a5c9f92396ea2
VRL00163087 <l00163087@student.lyit.ie>
2022-03-20 09:14:13 -0700

    updated hosted db details

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
 # from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
+from os import getenv, path
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 mysql = MySQL(app)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'wedwckd'
+app.config['MYSQL_HOST'] = 'sql4.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql4480309'
+app.config['MYSQL_PASSWORD'] = 'ywtgHyDEsc'
+app.config['MYSQL_DB'] = 'sql4480309'
+# app.config['MYSQL_PORT'] = 3306
 
 @app.route('/')
 def index():
@@ -26,10 +29,10 @@ def login_signup():
         password = request.args.get('password')
         cursor = mysql.connection.cursor()
         print('email',email, 'password', password)
-        cursor.execute('''select * from cust_details where email=%s and Password=%s''',(email,password))
+        cursor.execute('''select * from customer_details where email=%s and password=%s''',(email,password))
         result = cursor.fetchone()
-        # print('result',result)
-        if None != result and email == result[2] and password == result[3]:
+        print('result',result)
+        if None != result and email == result[1] and password == result[2]:
             mysql.connection.commit()
             cursor.close()
             return render_template('profile.html')
@@ -39,15 +42,14 @@ def login_signup():
             return '<h1>not matched</h1>'
 
     if request.method == 'POST':
-        firstname = request.args.get('su-name')
-        lastname=request.args.get('su-lastname')
-        Email = request.args.get('su-mail')
-        Password = request.args.get('su-pass')  
+        fullname = request.form.get('su-name')
+        email = request.form.get('su-mail')
+        password = request.form.get('su-pass')  
         # print(Email)
         # print(Password)
         # print(lastname)
         cursor = mysql.connection.cursor()
-        cursor.execute('''INSERT INTO cust_details VALUES(%s,%s,%s,%s)''',(firstname,lastname,Email,Password))
+        cursor.execute('''INSERT INTO customer_details (fullname, email, password) VALUES (%s,%s,%s)''',(fullname, email, password))
         mysql.connection.commit()
         cursor.close()
         return '</h1>signed up</h2>'    
@@ -55,3 +57,6 @@ def login_signup():
 if __name__ == '__main__':
     app.debug = True
     app.run()
+    # basedir = path.abspath(path.dirname(__file__))
+    # load_dotenv(path.join(basedir[:basedir.rindex('\\')], "dev.env"))  # Load environment variables from dev.env
+    # remote_host_address = getenv("REMOTE_HOST")  # get remote host environment variable
